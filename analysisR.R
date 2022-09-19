4a8e53a9834c327939a416002d23a8f185dc1275
David Canal <dav.canal.p@gmail.com>
2022-09-19 16:41:01 +0200

    new plot #close1

--- analysisR.R
+++ analysisR.R
@@ -1,2 +1,9 @@
 library(tidyverse)
-datos<-read_csv("github_data.csv")
+datos<-read_csv("github_data.csv") 
+#dataset: indice de horas trabajadas con felicidad
+glimpse(datos)
+#hago click en git ->commit. Luego en cada archivo:
+#commit : copia local
+#push: sincronizo con la nube
+
+plot (happiness ~work.hours, data=datos )
