01f3242532fe4525877f16ee7829825a2b6e0a19
Raúl Sánchez García <raul.sangar98@gmail.com>
2022-09-22 16:19:34 +0200

    He probado algunos modelos, pero ninguno funciona

--- code/preparedata.R
+++ code/preparedata.R
@@ -1,10 +1,39 @@
-#aqu? preparamos los datos
+library(performance)
+library(knitr)
+library(dplyr)
+library(see)
+library(parameters)
 
+#Primero metemos los datos
 
 ficed<-read.table(file="data/data-raw/minificed_curso.txt", header=T,sep="\t",na.string="NA",dec=".")
 str(ficed)
 
+#Seleccionamos las variables con las que queremos trabajar
+
 minificed<-subset(ficed, select=c("NP", "TP", "NPUPAS", "PUPASCUAL","EDREALH","EDREALM", "Year"))
 str(minificed)
 
+#Hacemos un primera visualización general de correlaciones
+
 pairs(minificed)
+plot(minificed)
+
+#Probamos algunos modelos con sentido biológico
+
+m1 <- lm(TP ~ EDREALH, data = minificed)
+summary(m1)
+plot(m1)
+
+m2 <- lm(TP ~ EDREALM, data = minificed)
+summary(m2)
+plot(m2)
+
+m3 <- lm(TP ~ Year, data = minificed)
+summary(m3)
+plot(m3)
+
+#Comparamos los modelos anteriores
+
+compare_performance(m1,m2,m3)
+plot(compare_performance(m1,m2,m3))
