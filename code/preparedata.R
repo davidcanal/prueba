9ee005fe79c4777da56d1a19be24df92526e978f
davidcanal <dav.canal.p@gmail.com>
2022-09-22 13:38:31 +0200

    import data

--- code/preparedata.R
+++ code/preparedata.R
@@ -1,1 +1,10 @@
-#aquí preparamos los datos
+#aqu? preparamos los datos
+
+
+ficed<-read.table(file="data/data-raw/minificed_curso.txt", header=T,sep="\t",na.string="NA",dec=".")
+str(ficed)
+
+minificed<-subset(ficed, select=c("NP", "TP", "NPUPAS", "PUPASCUAL","EDREALH","EDREALM", "Year"))
+str(minificed)
+
+                                
